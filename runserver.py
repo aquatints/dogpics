71740027fd490fd8ff9e34e50e1d39bda5356165
s3cpat <s3cpat@gmail.com>
2018-04-07 20:24:30 +0000

    update to be port 80

--- runserver.py
+++ runserver.py
@@ -6,9 +6,10 @@ from os import environ
 from dogpics_web import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
+    # HOST = environ.get('SERVER_HOST', 'localhost')
+    HOST = '0.0.0.0'
     try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
+        PORT = int(environ.get('SERVER_PORT', '80'))
     except ValueError:
-        PORT = 5555
+        PORT = 80
     app.run(HOST, PORT)
