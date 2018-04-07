c951a900983af58762490b0b87350a4b3c6006f3
s3cpat <s3cpat@gmail.com>
2018-04-07 22:20:50 +0000

    add placeholder functionality

--- dogpics_web/views.py
+++ dogpics_web/views.py
@@ -1,7 +1,8 @@
 """
 Routes and views for the flask application.
 """
-
+import os
+import random
 from datetime import datetime
 from flask import render_template
 from dogpics_web import app
@@ -9,29 +10,42 @@ from dogpics_web import app
 @app.route('/')
 @app.route('/home')
 def home():
-    """Renders the home page."""
-    return render_template(
-        'index.html',
-        title='Home Page',
-        year=datetime.now().year,
-    )
+	"""Renders the home page."""
+	return render_template(
+		'index.html',
+		title='DOGPICS',
+		#dog='static/content/img/placeholder.jpg',
+		dog=getpics(),
+		#debug=getpics(),
+		year=datetime.now().year,
+	)
 
 @app.route('/contact')
 def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
+	"""Renders the contact page."""
+	return render_template(
+		'contact.html',
+		title='Contact',
+		year=datetime.now().year,
+		message='Your contact page.'
+	)
 
 @app.route('/about')
 def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
+	"""Renders the about page."""
+	return render_template(
+		'about.html',
+		title='About',
+		year=datetime.now().year,
+		message='Your application description page.'
+	)
+
+def getpics():
+	""" Return a picture """
+	# temporarily return random picture from static/content/img/
+	path = '/dogpics/web/dogpics/dogpics_web/static/content/img/'
+	webpath = '/static/content/img/'
+	debug = os.listdir(path)
+	items = os.listdir(path)
+	picked = random.choice(items)
+	return webpath+picked;
