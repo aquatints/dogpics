e418a3693b9812d014cdf50723b3488bf4a4ac8d
s3cpat <s3cpat@gmail.com>
2018-04-07 23:10:15 -0400

    finished web interface

--- dogpics_web/views.py
+++ dogpics_web/views.py
@@ -11,33 +11,15 @@ from dogpics_web import app
 @app.route('/home')
 def home():
 	"""Renders the home page."""
+	pic = getpics()
 	return render_template(
 		'index.html',
 		title='DOGPICS',
 		#dog='static/content/img/placeholder.jpg',
-		dog=getpics(),
+		dog=pic,
+		srclink=getImgurLink(pic),
 		#debug=getpics(),
 		year=datetime.now().year,
-	)
-
-@app.route('/contact')
-def contact():
-	"""Renders the contact page."""
-	return render_template(
-		'contact.html',
-		title='Contact',
-		year=datetime.now().year,
-		message='Your contact page.'
-	)
-
-@app.route('/about')
-def about():
-	"""Renders the about page."""
-	return render_template(
-		'about.html',
-		title='About',
-		year=datetime.now().year,
-		message='Your application description page.'
 	)
 
 def getpics():
@@ -45,7 +27,15 @@ def getpics():
 	# temporarily return random picture from static/content/img/
 	path = '/dogpics/web/dogpics/dogpics_web/static/content/img/'
 	webpath = '/static/content/img/'
-	debug = os.listdir(path)
+	# debug = os.listdir(path)
 	items = os.listdir(path)
 	picked = random.choice(items)
 	return webpath+picked;
+
+def getImgurLink(filename):
+	# get imgur link
+	print('DEBUG:' + filename)
+	cutstr = '/img/'
+	cutext = '.jpg'
+	imgurlink = filename[filename.index(cutstr)+len(cutstr):] #filename.index(cutext)]
+	return 'https://imgur.com/' + imgurlink
