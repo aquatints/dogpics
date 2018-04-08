bffd82899099bd029bdf984a59aadb0342057f7a
s3cpat <s3cpat@gmail.com>
2018-04-08 01:50:27 -0400

    watson can identify dogs!

--- wat.py
+++ wat.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import json
 import os
 from watson_developer_cloud import VisualRecognitionV3, WatsonApiException
+import sys
 
 url = 'https://gateway-a.watsonplatform.net/visual-recognition/api'
 api_key = 'f8c0c68b5a97fd9c15f3c4a4f4561511782689d5'
@@ -8,8 +11,33 @@ api_key = 'f8c0c68b5a97fd9c15f3c4a4f4561511782689d5'
 visual_recognition = VisualRecognitionV3('2018-04-08', api_key=api_key)
 classifier_id = 'DOGSDOGSDOGSDOGS'
 
-with open('placeholder.jpg') as file:
-	possibleDoggo = file.read()
+confidence_threshold = 0.84
 
-result = visual_recognition.classify(images_file=possibleDoggo,threshold='0.1',classifier_ids=[classifier_id,'default'])
-print(json.dumps(result, indent=2))
+possibleDoggo='https://i.imgur.com/KPAnBFT.png' #default test
+
+if(len(sys.argv)>1):
+	possibleDoggo = sys.argv[1]
+
+url_result = visual_recognition.classify(parameters=json.dumps({'url': possibleDoggo}))
+
+data = url_result
+
+print(url_result)
+
+print()
+print()
+
+confidentDog = False
+
+for i in range(0,4):
+	if('dog' in data["images"][0]['classifiers'][0]['classes'][i]['class']) and (data["images"][0]['classifiers'][0]['classes'][i]['score']>=confidence_threshold):
+		confidentDog = True
+
+# does it contain dog?
+if(confidentDog):
+	dog = 'yes'
+else:
+	dog = 'no'
+
+print("Confident that this image contains a dog? " + dog)
+
