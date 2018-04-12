3bc9e8593e62c1aea13f2361b6a8fb6c561b0962
s3cpat <s3cpat@gmail.com>
2018-04-12 14:50:39 -0400

    add more debugging features

--- dogpics_web/views.py
+++ dogpics_web/views.py
@@ -2,6 +2,7 @@
 Routes and views for the flask application.
 """
 import os
+from os import walk
 import random
 from datetime import datetime
 from flask import render_template
@@ -23,9 +24,38 @@ def home():
 		year=datetime.now().year,
 	)
 
+@app.route('/debug')
+def debug():
+	"""Renders inventory page for debugging and admin purposes"""
+	allDogs = getAllDogs()
+	allNotDogs = getAllNotDogs()
+	allDiscarded = getAllDiscarded()
+	print_inventory = ""
+	print_notdogs = ""
+	print_discarded = ""
+	for i in allDogs:
+		print_inventory += "<form action=\"/report\" method='post'><img src=\"/static/content/img/" + i + "\" width=150px><button type='submit' class='btn btn-danger'>Not a dog</button><input type='hidden' name='reportimg' value='/static/content/img/" + i + "'><input type='hidden' name='reporturl' value='https://imgur.com/" + i + "'></form><br><hr><br>"
+	for i in allNotDogs:
+		print_notdogs += "<img src='https://imgur.com/" + i + "' width=150px><br>"
+	for i in allDiscarded:
+		print_discarded += "<img src='https://imgur.com/" + i + "' width=150px><br>"
+	return render_template(
+		'overview.html',
+		title='DOGPICS',
+		year=datetime.now().year,
+		inventory = print_inventory,
+		dogcount = len(allDogs),
+		discarded = print_discarded,
+		discardcount = len(allDiscarded),
+		notdog = print_notdogs,
+		notdogcount = len(allNotDogs)
+	)
+
+def moveToDogs():
+	#todo
+
 @app.route('/report', methods=['POST'])
 def report():
-	# todo report logic
 	return render_template(
 		'report.html',
 		title='NOT A DOG?',
@@ -38,7 +68,11 @@ def report():
 def reportConfirm():
 	# move to discard
 	f = '/dogpics/web/dogpics/dogpics_web'+ request.form['reportimg']
-	os.rename(f, '/dogpics/backend/discard/notadog.jpg')
+	cutstr = '/img/'
+	cutext = '.jpg'
+	name = request.form['reportimg']
+	name = name[name.index(cutstr)+len(cutstr):]
+	os.rename(f, '/dogpics/backend/notdogs/'+name)
 	"""Renders the home page."""
 	pic = getpics()
 	return render_template(
@@ -58,6 +92,32 @@ def getpics():
 	items = os.listdir(path)
 	picked = random.choice(items)
 	return webpath+picked;
+
+def getAllDogs():
+	""" Return array of all items in dog folder """
+	path = '/dogpics/web/dogpics/dogpics_web/static/content/img/'
+	webpath = '/static/content/img/'
+	dogs = []
+	for(dirpath, dirnames, filenames) in walk(path):
+		dogs.extend(filenames)
+		break
+	return dogs
+
+def getAllNotDogs():
+	""" Return array of items in user submitted 'not dogs' folder """
+	path = '/dogpics/backend/notdogs/'
+	notDogs = []
+	for(dirpath, dirnames, filenames) in walk(path):
+		notDogs.extend(filenames)
+	return notDogs
+
+def getAllDiscarded():
+	""" return array of items in discard folder """
+	path = '/dogpics/backend/discard/'
+	discarded = []
+	for(dirpath, dirnames, filenames) in walk(path):
+		discarded.extend(filenames)
+	return discarded
 
 def getImgurLink(filename):
 	# get imgur link
